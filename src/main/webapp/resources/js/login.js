87dd2aa1a71e6fa8045cd8fd760b23e513a67b17
dijalmasilva <dijalmacz@gmail.com>
2016-06-06 18:09:36 -0300

    Adicionando fotos e funcionalidades do sistema a página inicial

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -11,55 +11,24 @@ $(document).ready(function () {
 
     $(document).scroll(function () {
         posicaoScroll = $(document).scrollTop();
-        console.log(posicaoScroll);
+//        console.log(posicaoScroll);
 
         if (posicaoScroll === 0) {
             $('.header_login').animate({
                 height: '160px'
-            }, 20);
-            $('#nomePrefeitura').css({
-                display: 'block'
-            });
-            $('#imgPrefeitura').css({
-                width: '100px',
-                height: '100px'
+            }, 0);
+            $('#icon_iescolar').css({
+                marginTop: '50px'
             });
         } else if (posicaoScroll < 90) {
             var tamanho = 170 - posicaoScroll;
-
+            var marginIcon = 50 - (posicaoScroll/1.8);
             $('.header_login').animate({
                 height: tamanho + 'px'
-            }, 20);
-
-            if (posicaoScroll > 50) {
-                $('#nomePrefeitura').css({
-                    display: 'none'
-                });
-            }
-
-            if (posicaoScroll < 35) {
-                $('.prefeitura').css({
-                   margin: '15px'
-                });
-            }
-
-            if (posicaoScroll > 35 && posicaoScroll < 64) {
-                var imagem = 128 - posicaoScroll;
-                $('#imgPrefeitura').animate({
-                    width: imagem + 'px',
-                    height: imagem + 'px',
-                    padding: '-=1px'
-                }, 20);
-                $('.prefeitura').css({
-                   margin: '5px'
-                });
-            } 
-            
-            if (posicaoScroll > 63){
-                $('#imgPrefeitura').css({
-                    marginLeft: '4px'
-                })
-            }
+            }, 0);
+            $('#icon_iescolar').animate({
+                marginTop: marginIcon + 'px'
+            }, 0);
         }
     });
 });
