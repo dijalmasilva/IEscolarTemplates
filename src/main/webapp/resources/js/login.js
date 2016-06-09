32c57394daa8048d78efb466b1c47707071c34db
dijalmasilva <dijalmacz@gmail.com>
2016-06-08 23:57:54 -0300

    Página inicial quase finalizada

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -7,13 +7,14 @@
 
 $(document).ready(function () {
 
+    animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
     var posicaoScroll = 0;
     valorIconDown = -10;
 
     $(window).scroll(function () {
         posicaoScroll = $(this).scrollTop();
         console.log(posicaoScroll);
-        
+
 //        Função para rolagem do header
         if (posicaoScroll === 0) {
             $('.header_login').animate({
@@ -40,30 +41,41 @@ $(document).ready(function () {
                 marginTop: marginIcon + 'px'
             }, 0);
         }
-        
+
         //efeito do iescolar
-        if (posicaoScroll > 150){
+        if (posicaoScroll > 150) {
             $('#iescolar').addClass('animated rotateInDownLeft');
         }
-        
+
 //        efeito das funções 
-        if (posicaoScroll > 490){
+        if (posicaoScroll > 490) {
             $('.effect').addClass('animated bounceInUp');
         }
-        
-        if (posicaoScroll > 1600){
+
+        //efeito redes sociais
+        if (posicaoScroll > 1100) {
+            $('.row_custom').addClass('animated zoomIn');
+        }
+
+        //efeito coruja rodapé
+        if (posicaoScroll > 1600) {
             $('.icon-footer').addClass('animated slideInLeft');
         }
+
+    });
+    
+    $('.modal_conteudo').one(animationend, function (){
+       $(this).removeClass('animated zoomInUp');
+    });
+    
+    $('.modal_start').click(function (){
+       $(this).addClass('invisible') ;
+       $('.modal_dark').addClass('invisible');
     });
 });
 
-function efeitoFocus(hash, efeito) {
-
-    var animationName = "animated" + " " + efeito;
-    var animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    
-    $(hash).addClass(animationName).one(animationend, function () {
-        $(hash).removeClass(animationName);
-    });
-
+function abrirModalLogin(){
+    $('.modal_dark').removeClass('invisible');
+    $('.modal_start').removeClass('invisible');
+    $('.modal_conteudo').addClass('animated zoomInUp');
 }
